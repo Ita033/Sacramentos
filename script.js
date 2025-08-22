6e5e2bfe998641a25f12018dca54966241f17a68
Ita033 <163354642+Ita033@users.noreply.github.com>
2025-08-22 00:41:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(data => {
             const nombreElemento = document.getElementById('nombre-catequizado');
+            const rezaTexto = document.getElementById('reza-text');
             nombreElemento.textContent = data.name;
+            rezaTexto.textContent = data.message;
         })
         .catch(error => {
             console.error('Error al obtener el nombre:', error);
             const nombreElemento = document.getElementById('nombre-catequizado');
+            const rezaTexto = document.getElementById('reza-text');
             nombreElemento.textContent = 'Ocurrió un error';
+            rezaTexto.textContent = 'Ocurrió un error';
         });
 });
